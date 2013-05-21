7f4ada464501a209a254fa7d5910bcb7d1b1a14d
Allen <allenj@igskbacbmac05.local>
2013-05-20 23:21:14 -0600

    adding ItemsResult and Items service and using those to populat collections

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,42 @@
 
 /* Controllers */
 
-angular.module('explorer.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('explorer.controllers', [])
+    .controller('CollectionsCtrl', [ '$scope', '$http', 'ItemsResult', function ($scope, $http, ItemsResult) {
+        var itemsResult = ItemsResult.query({
+            parentId: '5137a368e4b066106b2eb640',
+            fields: 'title,summary'
+        }, function() {
+            var collections = itemsResult.items;
+            // split the items in half into 2 columns
+            var half = Math.ceil(collections.length / 2);
+            $scope.col1 = collections.slice(0, half);
+            $scope.col2 = [];
+            if (half > 1) {
+                $scope.col2 = collections.slice(half, collections.length);
+            }
+        })
 
-  }])
-  .controller('MyCtrl2', [function() {
+        $scope.search = function(query) {
+            var itemsResult = ItemsResult.query({
+                folderId: '5137a368e4b066106b2eb640',
+                fields: 'id,ancestors'
+            })
+        }
+        //$scope.collectionsUrl = "https://www.sciencebase.gov/catalog/items?parentId=5137a368e4b066106b2eb640&format=json&fields=title,summary";
 
-  }]);
+        // $http.get($scope.collectionsUrl, {cache: true}).success(function(data) {
+        //     var collections = data.items;
+        //     var half = Math.ceil(collections.length / 2);
+        //     $scope.col1 = collections.slice(0, half);
+        //     $scope.col2 = [];
+        //     if (half > 1) {
+        //         $scope.col2 = collections.slice(half, data.length);
+        //     }
+        // });
+    }])
+    .controller('SlideCtrl', [ '$scope', '$http', function ($scope, $http) {
+        $http.get('collections/slides.js').success(function(data) {
+            var slides = data;
+        });
+    }]);
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,15 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('explorer.services', []).
-  value('version', '0.1');
+angular.module('explorer.services', ['ngResource']).
+    factory('ItemsResult', function($resource) {
+        return $resource('https://www.sciencebase.gov/catalog/items', {}, {
+            query: {method: 'GET'}
+        });
+    })
+    .factory('Item', function($resource) {
+        return $resource('https://www.sciencebase.gov/catalog/item/:itemId', {itemId: '@id'}, {
+            get: {method: 'GET'}
+        });
+    })
+    .value('version', '0.1');
