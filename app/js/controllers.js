323cd159d87b494d004e603996d78fc000c438b6
allenj <allenj@usgs.gov>
2013-06-18 14:39:11 -0600

    Moving Search functionality into SearchService

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,6 +7,7 @@ var COLLECTIONS_PARENT_ID = "5137a368e4b066106b2eb640";
 
 angular.module('explorer.controllers', [])
     .controller('CollectionsCtrl', [ '$scope', 'SearchService', function ($scope, SearchService) {
+        SearchService.setPlaceHolder('Search Data Collections');
         SearchService.searchParams['parentId'] = COLLECTIONS_PARENT_ID;
         SearchService.fields = ['title', 'summary'];
 
@@ -20,87 +21,56 @@ angular.module('explorer.controllers', [])
         });
 
         SearchService.search();
-        
-        // var itemsResult = ItemsResult.query({
-        //     parentId: COLLECTIONS_PARENT_ID,
-        //     fields: 'title,summary'
-        // }, function() {
-        //     var collections = itemsResult.items;
-        //     // split the items in half into 2 columns
-        //     var half = Math.ceil(collections.length / 2);
-        //     $scope.col1 = collections.slice(0, half);
-        //     $scope.col2 = [];
-        //     if (half > 1) {
-        //         $scope.col2 = collections.slice(half, collections.length);
-        //     }
-        // });
-
-        // $scope.search = function(query) {
-        //     var itemsResult = ItemsResult.query({
-        //         folderId: COLLECTIONS_PARENT_ID,
-        //         fields: 'id,ancestors,parentId',
-        //         q: query
-        //     }, function() {
-
-        //     });
-        // };
     }])
-    .controller('SearchCtrl', [ '$scope', '$routeParams', '$location', 'ItemsResult', 'Item', '$rootScope', function ($scope, $routeParams, $location, ItemsResult, Item, $rootScope) {
+    .controller('NavCtrl', [ '$scope', 'SearchService', function ($scope, SearchService) {
+        $scope.doSearch = function() {
+            SearchService.search($scope.q);
+        };
+
+        $scope.$on('clear_search', function() {
+            $scope.q = '';
+        });
+    }])
+    .controller('SearchCtrl', [ '$scope', '$routeParams', '$location', 'SearchService', '$rootScope', function ($scope, $routeParams, $location, SearchService, $rootScope) {
+        SearchService.setPlaceHolder('Search Data');
+        SearchService.fields = ['title', 'summary', 'distributionLinks'];
+        SearchService.facets = ['browseCategory', 'browseType', 'partyWithName', 'facets.facetName', 'tagNameForTypeAndScheme'];
+        SearchService.filters = {'ancestors': $routeParams.parentId};
+
         $scope.parentId = $routeParams.parentId;
         $scope.items = [];
-        $scope.facets = [];
-        $scope.filter = "";
-
-        $rootScope.$watch('q', function(newVal, oldVal) {
-            $scope.q = newVal;
-        });
-
-        var loadItems = function(query) {
-            var itemsResult = ItemsResult.query(query, function() {
-                $scope.items = itemsResult.items;
-                $scope.facets = itemsResult.searchFacets;
-            });
-        };
-
-        // Load Items
-        var query = {
-            filter: "ancestors=" + $routeParams.parentId,
-            fields: "title,summary,distributionLinks",
-            facets: "browseCategory,browseType,partyWithName,facets.facetName,tagNameForTypeAndScheme",
-            offset: 0,
-            max: 20,
-            q: $scope.q != null ? $scope.q : ""
-        };
-        $.extend(query, $location.search());
-        loadItems(query);
-
-        // Infinite scroll
+        $scope.itemsTotal = 0;
+        $scope.searchFacets = [];
         $scope.busy = false;
         $scope.offset = 0;
+        $scope.filterCount = 0;
+        $scope.queryParams = $.param($location.search());
+
+        $scope.$on('new_items', function() {
+            $scope.items = $scope.items.concat(SearchService.items);
+            $scope.itemsTotal = SearchService.itemsTotal;
+            $scope.searchFacets = SearchService.searchFacets;
+            $scope.busy = false;
+            $scope.filterCount = SearchService.filterCount();
+        });
+
+        SearchService.searchParams = $location.search();
+
+        $rootScope.$broadcast('clear_search');
+        SearchService.search();
+
+        // Infinite scroll
         $scope.nextPage = function() {
             if ($scope.busy) return;
             $scope.busy = true;
             $scope.offset += 20;
-            query.offset = $scope.offset;
-            var scrollResults = ItemsResult.query(query, function() {
-                $scope.items = $scope.items.concat(scrollResults.items);
-                $scope.facets = scrollResults.searchFacets;
-                $scope.busy = false;
-            });
+            SearchService.searchParams['offset'] = $scope.offset;
+            SearchService.search();
         };
 
-        // Search
-        $scope.doSearch = function() {
-            var query = {
-                filter: "ancestors=" + $routeParams.parentId,
-                fields: "title,summary,distributionLinks",
-                facets: "browseCategory,browseType,partyWithName,facets.facetName",
-                offset: 0,
-                max: 20,
-                q: $scope.q != null ? $scope.q : ""
-            };
-            $.extend(query, $location.search());
-            loadItems(query);
-        }
-
+        $scope.addFilter = function(facet, term) {
+            SearchService.filters[facet] = term;
+            SearchService.searchFacets
+            SearchService.search();
+        };
     }]);
