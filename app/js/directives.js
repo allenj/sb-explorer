ba3fe4087b8fd8d67f258e8bc601ac2679bd01fd
allenj <allenj@usgs.gov>
2013-06-19 14:21:07 -0600

    Clearing items when a new string search is made

--- app/js/directives.js
+++ app/js/directives.js
@@ -8,4 +8,12 @@ angular.module('explorer.directives', [])
         return function(scope, elm, attrs) {
         elm.text(version);
         };
+    }])
+    .directive('facet', ['SearchService', function(SearchService) {
+        return {
+            restrict: 'E',
+            controller: function($scope, $element, $attrs, $transclude, SearchService) {
+                console.log("facet added?");
+            }
+        };
     }]);
